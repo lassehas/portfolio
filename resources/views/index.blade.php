51886a969453f83aa66e57696995f3b221444321
lassehas <89258322+lassehas@users.noreply.github.com>
2023-04-07 00:23:46 +0200

    setup project to begin dev

---
+++ resources/views/index.blade.php
@@ -0,0 +1,8 @@
+<x-layout>
+    <x-slot name="title">Welcome</x-slot>
+
+    <x-slot name="content">
+        <h1>Welcome to the site!</h1>
+        <p>Here's some content.</p>
+    </x-slot>
+</x-layout>
