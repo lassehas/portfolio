b4d9c442eff01299eb54ce5d16da0a741ca0bf23
lassehas <89258322+lassehas@users.noreply.github.com>
2023-04-07 03:18:02 +0200

    Tailwind wont work with run dev, so CDN is added

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -9,8 +9,8 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-
+    <script src="https://cdn.tailwindcss.com"></script>
+    @vite('resources/css/app.css')
     @livewireStyles
 </head>
 
--- resources/views/cv.blade.php
+++ resources/views/cv.blade.php
@@ -5,7 +5,7 @@
         <x-navbar></x-navbar>
 
         <div class="flex justify-center py-5">
-            <iframe class="w-4/5 h-96 md:h-screen" src="/storage/CV.pdf" style="">
+            <iframe class="w-4/5 aspect-video h-96 md:h-screen" src="/storage/CV.pdf" frameborder="0" style="">
 
             </iframe>
         </div>
