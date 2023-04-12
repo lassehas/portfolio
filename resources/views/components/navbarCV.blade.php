edaa51f9a74e049dea097e5bc5a7cf8efa8f3a5d
lassehas <89258322+lassehas@users.noreply.github.com>
2023-04-12 13:51:27 +0200

    Removed wrong placed text

--- resources/views/components/navbarCV.blade.php
+++ resources/views/components/navbarCV.blade.php
@@ -1,12 +1,11 @@
 <nav class="bg-gray-900 border-gray-200 dark:bg-gray-900">
     <div class="p-3">
         <div class="items-center flex justify-end mx-auto">
-            <p>Menu</p>
+            <p class="text-white md:hidden">Menu</p>
             <button data-collapse-toggle="navbar-default" type="button"
                     class="flex place-items-end inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
                     aria-controls="navbar-default" aria-expanded="false">
                 <span class="sr-only">Open main menu</span>
-                <span class="text-white">Menu</span>
                 <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
                      xmlns="http://www.w3.org/2000/svg">
                     <path fill-rule="evenodd"
