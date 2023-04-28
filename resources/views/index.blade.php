1b4c86c4676897edf3dd7fd97c2da6b2917b64b0
lassehas <89258322+lassehas@users.noreply.github.com>
2023-04-28 14:10:48 +0200

    fixed some text

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,7 +6,7 @@
 
         <x-banner>
             <x-slot name="title">Lasse Haslund</x-slot>
-            <x-slot name="description">Ung mand som brænder for mit arbejde, og tager ære i det jeg laver</x-slot>
+            <x-slot name="description">Ung mand som brænder for mit arbejde, og lægger ære i det jeg laver</x-slot>
             <x-slot name="image">
                 /storage/round_profil.png
             </x-slot>
